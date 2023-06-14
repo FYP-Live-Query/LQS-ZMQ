e1b8f6f2ea3f77ae78c082162a339916a34f792f
Mahesh <58337761+MaheshMadushan@users.noreply.github.com>
2023-06-14 23:31:06 +0530

    Fixing minor bugs in the CBR algorithm.

--- ZMQServer/src/main/java/com/mahesh/publisher/IStreamingEngine.java
+++ ZMQServer/src/main/java/com/mahesh/publisher/IStreamingEngine.java
@@ -6,5 +6,5 @@ package com.mahesh.publisher;
 public interface IStreamingEngine<T> {
     void consumeMessage(java.util.function.Consumer<T> consumer); // this method will use by stream thread to consume message (strategy design pattern execute method)
     void subscribe(); // this method will use by stream thread to subscribe to topic (strategy design pattern execute method)
-    void unsubscribe(); // this method will use by stream thread to unsubscribe from topic (strategy design pattern execute method)
+    void unsubscribe(); // this method will use by stream thread to unsubscribe from topic (strategy design pattern execute method) new
 }
